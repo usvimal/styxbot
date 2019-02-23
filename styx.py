e78ca93ba845303d321452cf62fb34ef88ea13a6
usvimal <usvimal@gmail.com>
2019-02-23 13:00:37 +0800

    Add updater to receive messages

--- styx.py
+++ styx.py
@@ -1,7 +1,9 @@
 import telegram
 import os
+from telegram.ext import Updater 
 
 TOKEN = os.environ.get("token")
+updater = Updater ( token = TOKEN )
 bot = telegram.Bot ( token = TOKEN )
 
 print(bot.get_me())
