15834c95916838a09ae37819e0f134dfea8634c0
usvimal <usvimal@gmail.com>
2019-02-24 19:47:24 +0800

    /start stuff and logging

--- styx.py
+++ styx.py
@@ -1,9 +1,42 @@
 import telegram
 import os
-from telegram.ext import Updater 
+import logging
+from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+					level=logging.INFO)
+
+log = logging.getLogger(__name__)
 
 TOKEN = os.environ.get("token")
-updater = Updater ( token = TOKEN )
-bot = telegram.Bot ( token = TOKEN )
+bot = telegram.Bot(token=TOKEN)
 
 print(bot.get_me())
+
+
+def start(update, context):
+	update.message.reply_text("Hello there, General Kenobi")
+
+
+def error(update, context):
+	"""Log Errors caused by Updates."""
+	log.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+	updater = Updater(token=TOKEN)
+	dispatcher = updater.dispatcher
+
+	dispatcher.add_handler(CommandHandler('start', start))
+
+	# log all errors
+	dispatcher.add_error_handler(error)
+
+	# start the bot
+	updater.start_polling()
+
+	updater.idle()
+
+
+if __name__ == '__main__':
+	main()
